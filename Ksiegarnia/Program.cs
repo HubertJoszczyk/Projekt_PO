acd599b6bee0a0a06e3341ac2306da204fc974eb
Hubert Joszczyk <86940@student.pb.edu.pl>
2024-05-03 22:01:24 +0200

    Podstawowy zarys projektu

--- Ksiegarnia/Program.cs
+++ Ksiegarnia/Program.cs
@@ -1,13 +1,37 @@
-﻿namespace Pracownik_sklepu{
-    class Pracownik_sklepu{
-
+﻿//Klasa dla pracownika sklepu (zarzadzanie stanem magazynowym oraz obsluga zamowien)
+class Pracownik_sklepu{
+    private List<Ksiazka> dostepne_Ksiazki = new List<Ksiazka>();
+    private List<Zamowienia> zamowienia = new List<Zamowienia>();   //tutaj trzeba będzie zrobić klase zamówienia gdzie będzie "tytuł książki/status wysłania"
+    public void Dodaj_do_oferty(List<Ksiazka> Ksiazki){
+        dostepne_Ksiazki.AddRange(Ksiazki);
+    }
+    public void Dodaj_zamowienie(Zamowienia zamowienia){
+        zamowienia.Add(zamowienia);
+        Console.WriteLine("Złożono zamówienie");
+        
     }
 }
-namespace Klient{
-    class Klient{
+//Klasa do przechowywania statusu zamowienia
+class Zamowienia{
+    private List<Ksiazka> Ksiazki = new List<Ksiazka>();
+    private bool Czy_wyslane ;
+}
+//Klasa dla klienta (przegladanie oferty i skladanie zamowienia)
+class Klient{
 
-    }
 }  
+//Klasa dla ksiazek
+class Ksiazka{
+    private string Tytul;
+    private string Autor;
+    private string Kategoria;
+    private string Format;
+    private double Cena;
+    private bool Dostepnosc;
+        public Ksiazka(){
+
+        }
+    }
 interface Program
 {
     
